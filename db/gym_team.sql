04c0a20a204c0c4b5713693d3d8807c3358d48aa
Emil <emil.shiva@gmail.com>
2021-07-12 12:19:21 +0100

    first commit - created db

---
+++ db/gym_team.sql
@@ -0,0 +1,19 @@
+DROP TABLE bookings;
+DROP TABLE gym_classes;
+DROP TABLE members;
+
+CREATE TABLE members (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE gym_classes (
+    id SERIAL PRIMARY KEY,
+    class_name VARCHAR(255)
+);
+
+CREATE TABLE bookings (
+    id SERIAL PRIMARY KEY,
+    member_id INT REFERENCES members(id) ON DELETE CASCADE,
+    gym_class_id INT REFERENCES gym_classes(id) ON DELETE CASCADE
+);
