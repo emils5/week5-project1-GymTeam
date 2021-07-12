be2b92f0fdcf2a45cb409dbfbccc13dcd743c8e7
Emil <emil.shiva@gmail.com>
2021-07-12 15:36:17 +0100

    repositories

---
+++ repositories/gym_class_repository.py
@@ -0,0 +1,21 @@
+from db.run_sql import run_sql
+
+from models.gym_class import Gym_class
+from models.member import Member
+
+def save(gym_class):
+    sql = "INSERT INTO gym_classes( name ) VALUES ( %s ) RETURNING id"
+    values = [gym_class.name]
+    results = run_sql(sql, values)
+    gym_class.id = results[0]['id']
+    return gym_class
+
+def select_all():
+    gym_classes = []
+
+    sql = "SELECT * FROM gym_classes"
+    results = run_sql(sql)
+    for row in results:
+        gym_class = Gym_class(row['name'], row['id'])
+        gym_classes.append(gym_class)
+    return gym_classes
---
+++ repositories/member_repository.py
@@ -0,0 +1,25 @@
+from db.run_sql import run_sql
+
+from models.gym_class import Gym_class
+from models.member import Member
+
+def save(member):
+    sql = "INSERT INTO members( name ) VALUES ( %s ) RETURNING id"
+    values = [member.name]
+    results = run_sql(sql, values)
+    member.id = results[0]['id']
+    return member
+
+def select_all():
+    members = []
+
+    sql = "SELECT * FROM members"
+    results = run_sql(sql)
+    for row in results:
+        member = Member(row['name'], row['id'])
+        members.append(member)
+    return members
+
+
+
+
